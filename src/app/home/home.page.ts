0c8270808661df4a09a298d13d4dd18d27c00832
Damian Tarnawsky <damian@ionic.io>
2022-05-26 12:30:28 -0700

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { LogDevice, LogStatus, RemoteLogger } from 'plugins/remote-logger';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor() { }
 
+  ngOnInit() {
+    RemoteLogger.hostName = 'localhost';
+    RemoteLogger.port = 8942;
+    RemoteLogger.addListener('logStatusChange', (status: LogStatus) => {
+      console.log('status is ', status);
+    });
+
+    RemoteLogger.initialize();
+    RemoteLogger.log({level: 'info', message: 'hi'});
+  }
 }
