3fe5574a9350cb74c4813376eced6974967537d3
Damian Tarnawsky <damian@ionic.io>
2022-05-27 09:39:58 -0700

    Rebuilt

--- packages/remote-logger-plugin/dist/esm/index.d.ts
+++ packages/remote-logger-plugin/dist/esm/index.d.ts
@@ -1,5 +1,4 @@
 import type { CapacitorRemoteLoggerPlugin } from './definitions';
-
 declare const RemoteLogger: CapacitorRemoteLoggerPlugin;
 export * from './definitions';
 export { RemoteLogger };
--- packages/remote-logger-plugin/dist/esm/index.js
+++ packages/remote-logger-plugin/dist/esm/index.js
@@ -1,6 +1,6 @@
 import { registerPlugin } from '@capacitor/core';
 const RemoteLogger = registerPlugin('RemoteLogger', {
-  web: () => import('./web').then(m => new m.RemoteLoggerWeb()),
+    web: () => import('./web').then(m => new m.RemoteLoggerWeb()),
 });
 export * from './definitions';
 export { RemoteLogger };
